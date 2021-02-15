c3c8bd542b930c7ff80598dec9458d952f97381c
Taufik Alam <taufikalam759@gmail.com>
2021-02-15 23:28:58 +0530

    Project completed

--- John-restaurant/src/app/app-routing.module.ts
+++ John-restaurant/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ContactComponent } from './dashboard/contact/contact.component';
-import { HomeComponent } from './dashboard/home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- John-restaurant/src/app/app.module.ts
+++ John-restaurant/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './dashboard/home/home.component';
-import { ContactComponent } from './dashboard/contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
